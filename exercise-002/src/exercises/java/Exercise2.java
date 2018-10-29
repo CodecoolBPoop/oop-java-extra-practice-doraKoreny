89f29864da5cc16c3d7212b0460e81d75cba463e
doraKoreny <dorakoreny@gmail.com>
2018-10-29 21:46:21 +0100

    Declared another int variable, initialized the sum of two integers, print the result

--- exercise-002/src/exercises/java/Exercise2.java
+++ exercise-002/src/exercises/java/Exercise2.java
@@ -4,6 +4,7 @@ public class Exercise2 {
     public static void main(String[] args) {
         int num1 = 2;
         int num2 = 3;
-
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
