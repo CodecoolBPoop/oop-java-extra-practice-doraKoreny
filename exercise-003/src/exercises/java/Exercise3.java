8f43a53ea1c9d7d9e51db436a009d14e425066e5
doraKoreny <dorakoreny@gmail.com>
2018-10-30 10:57:25 +0100

    Exercise3, get the sum of array elements with for loop

--- exercise-003/src/exercises/java/Exercise3.java
+++ exercise-003/src/exercises/java/Exercise3.java
@@ -4,5 +4,9 @@ public class Exercise3 {
     public static void main(String[]args) {
         int[] numbers = {1,2,3,4,5};
         int sum = 0;
+        for(int element:numbers) {
+            sum += element;
+        }
+        System.out.println(sum);
     }
 }
