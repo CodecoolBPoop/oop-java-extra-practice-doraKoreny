36c00a0e8d4885fbb52244cb9005c4cc2bf02de9
doraKoreny <dorakoreny@gmail.com>
2018-10-30 10:28:25 +0100

    Exercise2, added longs and floats

--- exercise-002/src/exercises/java/Exercise2.java
+++ exercise-002/src/exercises/java/Exercise2.java
@@ -4,7 +4,17 @@ public class Exercise2 {
     public static void main(String[] args) {
         int num1 = 2;
         int num2 = 3;
-        int result = num1 + num2;
-        System.out.println(result);
+        int intResult = num1 + num2;
+        System.out.println(intResult);
+
+        long long1 = 1000000000;
+        long long2 = 2000000000;
+        long longResult = long1+long2;
+        System.out.println(longResult);
+
+        float float1 = 199.9f;
+        float float2 = 56.2f;
+        float floatResult = float1 + float2;
+        System.out.println(floatResult);
     }
 }
